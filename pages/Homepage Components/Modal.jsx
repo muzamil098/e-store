a4258964b8bbad3ecbce43dfdfe018c9a02f722d
Muzammil Channa <muzamilkenvay@gmail.com>
2024-12-19 20:35:23 +0500

    modal

--- pages/Homepage Components/Modal.jsx
+++ pages/Homepage Components/Modal.jsx
@@ -13,7 +13,7 @@ function Modal(props) {
   return (
     <div className="absolute top-0 left-0 h-full  w-full  text-white  backdrop-blur-sm bg-white/30 z-[1000] flex justify-center items-center">
       <div className="bg-white text-black p-4 w-[50rem] rounded-md shadow-2xl z-[60] border">
-        <div className="w-full flex justify-end items-center">
+        <div className="w-full flex justify-end   items-center">
           <h4
             className="text-xl font-bold text-red-600 cursor-pointer"
             onClick={onIsCheckout}
